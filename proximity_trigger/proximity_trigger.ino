2a552674c424be724e17cee96c238633ca60265a
bfiegel <bfiegel@engineerweb.org>
2014-06-04 23:58:01 -0500

    Code now tested, hardware still needs ironing out.

--- proximity_trigger/proximity_trigger.ino
+++ proximity_trigger/proximity_trigger.ino
@@ -20,17 +20,21 @@
 
 #include <avr/wdt.h>
 
+#define USE_ENABLE_ULTRASONIC_SENSOR 0
+#define USE_ENABLE_LIGHT_SENSOR      1
 #define numberof(x) (sizeof((x))/sizeof 0[(x)])
 
+#if USE_ENABLE_LIGHT_SENSOR
 #define PHOTO_RESISTOR_PIN A0
+#endif
 
-#define ENABLE_DELAY_MS    (60 * 1000)
+#define ENABLE_DELAY_MS    (1 * 60 * 1000)
 #define ENABLE_DISTANCE_CM (10)
-#define ENABLE_PRESENCE_COUNTER_RESET (10)
-#define ENABLE_PRESENCE_COUNTER_INIT (0)
+#define ENABLE_PRESENCE_COUNTER_RESET (100)
+#define ENABLE_PRESENCE_COUNTER_INIT (1)
 
-#define TRIGGER_DISTANCE_CM (30)
-#define TRIGGER_PRESENCE_COUNTER_RESET (100)
+#define TRIGGER_DISTANCE_CM (20)
+#define TRIGGER_PRESENCE_COUNTER_RESET (5)
 #define TRIGGER_PRESENCE_COUNTER_INIT (0)
 
 #define TRIGGER_ABSENCE_COUNTER_RESET (10)
@@ -40,7 +44,7 @@ unsigned long enableStartTime;
 unsigned int  enablePresenceCounter  = ENABLE_PRESENCE_COUNTER_INIT;
 unsigned int  triggerPresenceCounter = TRIGGER_PRESENCE_COUNTER_INIT;
 unsigned int  triggerAbsenceCounter  = TRIGGER_ABSENCE_COUNTER_INIT;
-unsigned char relayPin[] = {7};
+unsigned char triggerPin[] = {4};
 
 class Ultrasonic
 {
@@ -80,7 +84,9 @@ long Ultrasonic::microsecondsToInches(void)
 	return duration/74/2;	
 }
 
+#if USE_ENABLE_ULTRASONIC_SENSOR
 Ultrasonic enableUltrasonic(10);
+#endif
 Ultrasonic triggerUltrasonic(9);
 
 void setup()
@@ -89,14 +95,38 @@ void setup()
   
   Serial.begin(115200);
   
-  for(i = 0; i < numberof(relayPin); ++i)
+  for(i = 0; i < numberof(triggerPin); ++i)
   {
-    pinMode(relayPin[i],OUTPUT);
-    digitalWrite(relayPin[i],LOW);
+    pinMode(triggerPin[i],OUTPUT);
+    digitalWrite(triggerPin[i],LOW);
   }
   
   enableStartTime = millis();
   wdt_enable (WDTO_2S);  // reset after two seconds, if no "pat the dog" received
+}
+
+void setTrigger()
+{
+  int i;
+  for(i = 0; i < numberof(triggerPin); ++i)
+  {
+    digitalWrite(triggerPin[i],HIGH);
+    delay(20); //delay between turning on each relay, 20 milliseconds
+    Serial.print("set relay ");
+    Serial.println(triggerPin[i]);
+  }
+}
+
+void resetTrigger()
+{
+  int i;
+  for(i = 0; i < numberof(triggerPin); ++i)
+  {
+    digitalWrite(triggerPin[i],LOW);
+    delay(20); //delay between turning on each relay, 20 milliseconds
+    Serial.print("reset relay ");
+    Serial.println(triggerPin[i]);
+  }
 }
 
 void loop()
@@ -108,46 +138,71 @@ void loop()
   long EnableRangeInCentimeters, TriggerRangeInCentimeters;
   
   wdt_reset ();
-  lightLevel = analogRead(PHOTO_RESISTOR_PIN); 
+#if USE_ENABLE_LIGHT_SENSOR
+  lightLevel = analogRead(PHOTO_RESISTOR_PIN);
+#endif
+#if USE_ENABLE_ULTRASONIC_SENSOR
   enableUltrasonic.DistanceMeasure();
-  triggerUltrasonic.DistanceMeasure();
   EnableRangeInCentimeters = enableUltrasonic.microsecondsToCentimeters();//convert the time to centimeters
+#endif
+  triggerUltrasonic.DistanceMeasure();
   TriggerRangeInCentimeters = triggerUltrasonic.microsecondsToCentimeters();//convert the time to centimeters
+#if 1
+  Serial.print("Enable Start Time: ");
+  Serial.println(enableStartTime);
+  Serial.print("Now: ");
+  Serial.println(millis());
+#if USE_ENABLE_ULTRASONIC_SENSOR
   Serial.print("Distance to enable object: ");
-  Serial.print(EnableRangeInCentimeters);//0~400cm
-  Serial.println(" cm");
-  Serial.print("Distance to trigger object: ");
   Serial.print(EnableRangeInCentimeters);//0~400cm
   Serial.println(" cm");
   Serial.print("Enable Counts: ");
   Serial.println(enablePresenceCounter);
+#endif
+#if USE_ENABLE_LIGHT_SENSOR
+  Serial.print("Light Level is: ");
+  Serial.println(lightLevel);
+#endif
+  Serial.print("Distance to trigger object: ");
+  Serial.print(TriggerRangeInCentimeters);//0~400cm
+  Serial.println(" cm");
   Serial.print("Trigger Counts: ");
   Serial.println(triggerPresenceCounter);
+#endif
 
   delay(10);
-  if ((EnableRangeInCentimeters <= ENABLE_DISTANCE_CM) || (lightLevel > 500))
+  if (
+#if !USE_ENABLE_ULTRASONIC_SENSOR && !USE_ENABLE_LIGHT_SENSOR
+  1
+#else
+# if USE_ENABLE_ULTRASONIC_SENSOR
+  (EnableRangeInCentimeters <= ENABLE_DISTANCE_CM)
+# elif USE_ENABLE_LIGHT_SENSOR
+  (lightLevel < 100)
+# else
+  0
+# endif
+#endif
+  )
   {
     if(0 == enablePresenceCounter)
     {
       time = millis();
   
       if (time < enableStartTime) 
-      {
+      {/* time has wrapped, just reset the start to 0 */
         enableStartTime = 0;
       }
       
-      if (time > enableStartTime)
+      if (time > (enableStartTime + ENABLE_DELAY_MS))
       {
         if (TriggerRangeInCentimeters <= TRIGGER_DISTANCE_CM)
         {
+          /* under the trigger threshold, set the trigger once this condition appears stable */
           if (0 == triggerAbsenceCounter)
           {
             triggerPresenceCounter = TRIGGER_PRESENCE_COUNTER_RESET;
-            for(i = 0; i < numberof(relayPin); i++)
-            {
-              digitalWrite(relayPin[i],HIGH);
-              delay(20); //delay between turning on each relay, 20 milliseconds
-            }
+            setTrigger();
           }
           else
           {
@@ -156,14 +211,11 @@ void loop()
         }
         else
         {
+          /* above the trigger theshold, reset the trigger once this condition appears stable */
           if (0 == triggerPresenceCounter)
           {
             triggerAbsenceCounter = TRIGGER_ABSENCE_COUNTER_RESET;
-            for(i = 0; i < numberof(relayPin); i++)
-            {
-              digitalWrite(relayPin[i],LOW);
-              delay(20); //delay between turning off each relay, 20 milliseconds
-            }
+            resetTrigger();
           } 
           else
           {
@@ -173,7 +225,7 @@ void loop()
       }
       else
       {
-        
+        /* Don't do anything until enabled for specified time period */  
       }
     }
     else
@@ -183,7 +235,8 @@ void loop()
   }
   else
   {
-    enableStartTime       = 0;
+    resetTrigger();
+    enableStartTime       = millis();
     enablePresenceCounter = ENABLE_PRESENCE_COUNTER_RESET;
   }  
 }
